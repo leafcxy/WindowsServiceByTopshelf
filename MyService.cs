fbfb4ca4932e6fbe67b6bd927bd222f609017447
leafcxy <cxy1337@gmail.com>
2024-06-27 14:59:26 +0800

    end

---
+++ MyService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using Topshelf;
+
+namespace WindowsServiceByTopshelf
+{
+    //public partial class MyService : ServiceBase
+    //{
+    //    public MyService()
+    //    {
+    //        InitializeComponent();
+    //    }
+
+    //    protected override void OnStart(string[] args)
+    //    {
+    //    }
+
+    //    protected override void OnStop()
+    //    {
+    //    }
+    //}
+
+    public class MyService
+    {
+        public void Start()
+        {
+            Console.WriteLine("Service started.");
+            Logs.Write("Service started.");
+
+            while (true)
+            {
+                Logs.Write("Service doing somethings.");
+                Thread.Sleep(2000);
+            }
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Service stopped.");
+            Logs.Write("Service stopped.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
+using Topshelf;
 
 namespace WindowsServiceByTopshelf
 {
@@ -12,14 +8,34 @@ namespace WindowsServiceByTopshelf
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static void Main()
+        static void Main(string[] args)
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
+            //ServiceBase[] ServicesToRun;
+            //ServicesToRun = new ServiceBase[]
+            //{
+            //    new MyService()
+            //};
+            //ServiceBase.Run(ServicesToRun);
+
+            HostFactory.Run(x =>
             {
-                new Service1()
-            };
-            ServiceBase.Run(ServicesToRun);
+                x.Service<MyService>(s =>
+                {
+                    s.ConstructUsing(name => new MyService());
+                    s.WhenStarted(service => service.Start());
+                    s.WhenStopped(service => service.Stop());
+                });
+
+                x.SetServiceName("MySuperService");
+                x.SetDisplayName("My Super Service");
+                x.SetDescription("This is an example of a Topshelf service.");
+
+                // 设置服务启动类型：手动、自动或禁用
+                x.RunAsLocalSystem(); // 或者使用其他账户，如NetworkService、LocalService等
+                x.StartAutomatically(); // 或者 StartManually、StartOnDemand
+
+                //x.UseNLog(); // 如果你使用NLog作为日志框架
+            });
         }
     }
 }
